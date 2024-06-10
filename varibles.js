6c96e005577cfc58ddb91c56169fe03ebe15df94
Comfort Ngwenya <>
2024-06-10 14:57:30 +0200

    Variables exercises

---
+++ varibles.js
@@ -0,0 +1,30 @@
+/*Exercise 1*/
+name = "Comfort"
+age = 28
+isStudent = true
+
+console.log("Name: " + name , "Age: " + age , "Student: " + isStudent)
+
+
+/*Exercise 2*/
+
+favoriteColour = "black"
+
+console.log("Fav Color: " +favoriteColour)
+
+favoriteColour = "grey"
+
+console.log("New Color: " +favoriteColour)
+
+
+/*Exercise 3*/
+
+num1 = 10
+num2 = 27
+
+sum = num1 + num2
+differnce = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+console.log("Sum: " + sum, "Difference: " + differnce, "Product: " + product, "Quotient: " + quotient)
