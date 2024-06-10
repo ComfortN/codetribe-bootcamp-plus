0e38e30070bfd9e89dc95f60f949dd175967dfab
Comfort Ngwenya <>
2024-06-10 15:50:46 +0200

    datatypes & operators exercises

---
+++ datatypes_operators.js
@@ -0,0 +1,64 @@
+/*Exercise 1*/
+
+num1 = 40
+num2 = 15.5
+
+Addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+modulus = num1 % num2
+exponentiation = num1 ** num2
+
+console.log("Add: " + Addition)
+console.log("Subtract: " + subtraction)
+console.log("Multoply: " + multiplication)
+console.log("Divide: " + division)
+console.log("Modulus: " + modulus)
+console.log('Exponent: ' + exponentiation)
+
+
+/*Exercise 2*/
+
+x = 8
+y = 12
+
+isGreater = x > y
+islessOrEqual = x <= y
+isEqual = x == y
+isNotEqual = x != y
+
+console.log("Greater: "  + isGreater)
+console.log("Less or Equal: " + islessOrEqual)
+console.log("Equal: " + isEqual)
+console.log("Not Equal: " + isNotEqual)
+
+
+a = true
+b= false
+
+and = a && b
+or = a || b
+not = a != b
+
+console.log("And: " + and)
+console.log("Or: " + or)
+console.log("Not: " + not)
+
+
+p = 10
+
+p += 8
+console.log("+= 8: " + p)
+
+p -= 8
+console.log("-= 8: " + p)
+
+p *= 8
+console.log("*= 8: " + p)
+
+p /= 8
+console.log("/= 8: " + p)
+
+p %= 8
+console.log("%= 8: " + p)
